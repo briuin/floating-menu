76e5ee761722795863c12d9a73fca4db89fea4cb
briuin <dreamingdexiaoxiaohao@gmail.com>
2022-04-13 23:44:50 +0800

    use '' for output

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -10,7 +10,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].js',
-    publicPath: './',
+    publicPath: '',
     chunkFilename: 'lib_[name].js',
   },
   optimization: {
